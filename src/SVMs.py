ec2651bcc1d52a88135229312137687c66a3f898
Venkata Kalyan Sagi <kalyansagi.555@gmail.com>
2022-07-06 10:19:47 -0400

    working model. still need tuning.

--- src/SVMs.py
+++ src/SVMs.py
@@ -10,28 +10,33 @@ y = dataset.iloc[:, 4].values
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
 
 # Feature Scaling
 from sklearn.preprocessing import StandardScaler
+
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
 
 # Fitting SVM classifier to the Training set
 from sklearn.svm import SVC
-#*******Missing line here**********************
-#*******Missing line here**********************
+
+classifier = SVC(kernel='linear', random_state=0)
+classifier.fit(X_train, y_train)
 
 # Predicting the Test set results
-#*******Missing line here**********************
+y_pred = classifier.predict(X_test)
 
 # Making the confusion Matrix
 from sklearn.metrics import confusion_matrix
-#*******Missing line here**********************
+
+cm = confusion_matrix(y_test, y_pred)
 
 # Visualizing the Training set results
 from matplotlib.colors import ListedColormap
+
 X_set, y_set = X_train, y_train
 X1, X2 = np.meshgrid(np.arange(start=X_set[:, 0].min() - 1, stop=X_set[:, 0].max() + 1, step=0.01),
                      np.arange(start=X_set[:, 1].min() - 1, stop=X_set[:, 1].max() + 1, step=0.01))
@@ -51,6 +56,7 @@ plt.show()
 
 # Visualizing the Test set results
 from matplotlib.colors import ListedColormap
+
 X_set, y_set = X_test, y_test
 X1, X2 = np.meshgrid(np.arange(start=X_set[:, 0].min() - 1, stop=X_set[:, 0].max() + 1, step=0.01),
                      np.arange(start=X_set[:, 1].min() - 1, stop=X_set[:, 1].max() + 1, step=0.01))
